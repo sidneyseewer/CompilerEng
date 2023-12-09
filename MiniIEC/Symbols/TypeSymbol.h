5ae9de3c1f6400cc2edfd4681f0d700108004ee3
Florian Gapp <s2220567015@fhooe.at>
2023-12-09 09:34:15 +0100

    added Symbols

---
+++ MiniIEC/Symbols/TypeSymbol.h
@@ -0,0 +1,10 @@
+#ifndef __TYPE_SYMBOL_H__
+#define __TYPE_SYMBOL_H__
+
+#include "Symbols/Symbol.h"
+class TypeSymbol: public Symbol
+{
+
+};
+
+#endif //!__TYPE_SYMBOL_H_
---
+++ MiniIEC/Symbols/VarSymbol.h
@@ -0,0 +1,10 @@
+#ifndef ___VAR_SYMBOL_H__
+#define ___VAR_SYMBOL_H__
+
+#include "Symbols/Symbol.h"
+class VarSymbol: public Symbol
+{
+
+};
+
+#endif //!__VAR_SYMBOL_H_
